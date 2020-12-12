87762704685dca6535287e328ffa63315311cfd8
Yangchenchen <yangchenchen.li@uq.net.au>
2020-12-12 17:14:39 +1000

    Update Exercise 2 code

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -2,17 +2,42 @@ import React, {Component} from 'react'
 
 export default class Main extends Component {
 
-    render(){
+    state = {
+        initView: true,
+        loading: false,
+        users: null,
+        errorMsg: null
+    }
 
-        return(
-            <div className="row">
-                <div className="card">
-                    <a href="https://github.com/reactjs" target="_blank">
-                        <img src="https://avatars.githubusercontent.com/u/6412038?v=3" style={{width: 100}}/>
-                    </a>
-                    <p className="card-text">reactjs</p>
+
+
+    render(){
+        const {initView, loading, users, errorMsg} = this.state
+
+        if (initView) {
+            return <h2>Please enter keyword to search</h2>
+        }else if (loading) {
+            return <h2>Loading ...</h2>
+        }else if (errorMsg) {
+            return <h2>{errorMsg}</h2>
+        }else {
+            return(
+                <div className="row">
+                    {
+                        users.map((user, index) => (
+                            <div className="card">
+                                <a href={user.url} target="_blank">
+                                    <img src={user.avatarUrl} style={{width: 100}}/>
+                                </a>
+                                <p className="card-text">{user.name}</p>
+                            </div>
+                        ))
+                    }
+
                 </div>
-            </div>
-        )
+            )
+
+        }
+
     }
 }
