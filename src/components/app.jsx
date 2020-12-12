3fa6896875967bc11075d5ce83bc175ee8547102
Yangchenchen <yangchenchen.li@uq.net.au>
2020-12-12 21:37:20 +1000

    Update Exercise 2 code

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -5,12 +5,22 @@ import Main from "./main";
 
 export default class App extends Component {
 
+    state = {
+        searchName: ''
+    }
+
+    setSearchName = (searchName) => {
+        // 更新状态
+        this.setState({searchName})
+
+    }
+
     render(){
 
         return(
             <div className="container">
-                <Search />
-                <Main />
+                <Search setSearchName={this.setSearchName}/>
+                <Main searchName={this.state.searchName}/>
             </div>
         )
     }
--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,6 +1,12 @@
 import React, {Component} from 'react'
+import PropTypes from 'prop-types'
+import axios from 'axios'
 
 export default class Main extends Component {
+
+    static propTypes = {
+        searchName:PropTypes.string.isRequired
+    }
 
     state = {
         initView: true,
@@ -9,13 +15,40 @@ export default class Main extends Component {
         errorMsg: null
     }
 
+    // 当组件接收到新的属性时，回调
+    componentWillReceiveProps(newProps) { //指定了新的searchName，需要请求
+        const {searchName} = newProps
+        // 更新状态 (请求中）
+        this.setState({
+            initView: false,
+            loading: true
+        })
+        // 发ajax请求
+        const url = `https://api.github.com/search/users?q=${searchName}`
+        axios.get(url)
+            .then(response => {
+                // 得到响应数据
+                const result = response.data
+                console.log(result)
+
+
+                // 更新状态（成功）
+
+            })
+            .catch(error => {
+                // 更新状态（失败）
+
+            })
+    }
 
 
     render(){
         const {initView, loading, users, errorMsg} = this.state
+        const {searchName} = this.props
+        console.log('render()', searchName)
 
         if (initView) {
-            return <h2>Please enter keyword to search</h2>
+            return <h2>Please enter keyword to search: {searchName}</h2>
         }else if (loading) {
             return <h2>Loading ...</h2>
         }else if (errorMsg) {
